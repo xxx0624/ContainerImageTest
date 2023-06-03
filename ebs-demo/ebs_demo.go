90a0c32da4b9ae2e5dd1254a578c2c1082050d76
Xing Zheng <xingzhen@amazon.com>
2023-06-02 18:06:34 -0700

    first commit

---
+++ ebs-demo/ebs_demo.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for true {
+		fmt.Println("Testing")
+		time.Sleep(10 * time.Second)
+	}
+}
---
+++ ebs-demo/go.mod
@@ -0,0 +1,3 @@
+module github.com/xxx0624/container-image-test/ebs-demo
+
+go 1.19
